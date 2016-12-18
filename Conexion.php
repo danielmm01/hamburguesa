47e8ba7c7d90a1e8373c4d4badb03d85e6c08f2a
moymiss <moymiss1@gmail.com>
2016-12-17 20:34:31 -0600

    limpiado jaja xD

--- Conexion.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conexion= new PDO('mysql:host=localhost;dbname=hamburguesa','Danny','danylgp350fmor');
-?>
--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("dsn","mysql:host=localhost;dbname=hamburguesa");
-define("usuario_bd", "Danny");
-define("pass_bd","danylgp350fmor");
-?>
